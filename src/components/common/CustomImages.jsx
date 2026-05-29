99d3f1f81a35234e7f6942569487861c402fab2a
mcjothykrishnan <mcjothykrishnan@gmail.com>
2026-05-29 10:05:18 +0530

    fix

--- src/components/common/CustomImages.jsx
+++ src/components/common/CustomImages.jsx
@@ -1,15 +1,14 @@
-import Logo from "../../../public/assets/cred_logo.png"
-import HeroPodium from "../../../public/assets/home/hero_podium.webp"
-import Pay from "../../../public/assets/home/pay.webp";
-import UPI from "../../../public/assets/home/upi.webp";
-import Coins from "../../../public/assets/home/coins.webp";
-import Reward from "../../../public/assets/home/reward.webp";
-import Score from "../../../public/assets/home/score.webp";
-import Cashback from "../../../public/assets/home/cashback.webp";
-import About from "../../../public/assets/home/about.webp";
-import Ios from "../../../public/assets/home/ios.webp";
-import Playstore from "../../../public/assets/home/playstore.webp";
-
+import Logo from "../../assets/credlogo.webp";
+import HeroPodium from "../../assets/home/heropodium.webp";
+import Pay from "../../assets/home/pay.webp";
+import UPI from "../../assets/home/upi.webp";
+import Coins from "../../assets/home/coins.webp";
+import Reward from "../../assets/home/reward.webp";
+import Score from "../../assets/home/score.webp";
+import Cashback from "../../assets/home/cashback.webp";
+import About from "../../assets/home/about.webp";
+import Ios from "../../assets/home/ios.webp";
+import Playstore from "../../assets/home/playstore.webp";
 
 export const CustomImages = {
   Logo,
